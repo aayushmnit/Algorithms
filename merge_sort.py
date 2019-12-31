fd59798fb8971af5c1920d1e3ff214b8a01295e4
aayushmnit <aayushmnit@gmail.com>
2019-12-30 16:15:32 -0800

    Formatting with black

--- merge_sort.py
+++ merge_sort.py
@@ -1,15 +1,15 @@
 def MergeSort(x):
-    '''
+    """
     Implementation of merge sort algorithm nlog(n) complexity
     x : list
-    '''
+    """
     # List of size 1 or empty list
     if len(x) <= 1:
         return x
         # Divide
 
     ## Find the middle point
-    n = int(len(x)/2)
+    n = int(len(x) / 2)
 
     y = x[:n]
     z = x[n:]
@@ -17,10 +17,10 @@ def MergeSort(x):
     # Recursion you beauty
     y = MergeSort(y)
     z = MergeSort(z)
-    
+
     # Merge logic
     finalOutput = []
-    
+
     ## Caculating length of arrays to track upper bound
     yLen = len(y)
     zLen = len(z)
@@ -30,10 +30,10 @@ def MergeSort(x):
 
     for k in range(len(x)):
         # Check if first list is completely in final output
-        if (i+1) > yLen:
+        if (i + 1) > yLen:
             return finalOutput + z[j:]
         # Check if second list is completely in final output
-        if (j+1) > zLen:
+        if (j + 1) > zLen:
             return finalOutput + y[i:]
 
         ## Sort two numbers, if tie take from first array
@@ -41,13 +41,8 @@ def MergeSort(x):
             finalOutput.append(y[i])
             i += 1
         else:
-            finalOutput.append(z[j])   
-            j += 1          
+            finalOutput.append(z[j])
+            j += 1
 
     return finalOutput
-
-
-
-    
-
 
