7ce35a8592c4f814063c2a6a0bea7ced88319b03
aayushmnit <aayushmnit@gmail.com>
2019-12-30 16:14:36 -0800

    Adding implementation of merge sort algorithm

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Algorithms implemented for algorithms course on [Coursera](https://www.coursera.org/specializations/algorithms?)
 
 ### 1) Divide and Conquer
-*1*) Karatsuba multiplication of two integers - [Link](https://github.com/aayushmnit/algorithms/blob/master/karatsuba_multiplication.py)
+1. Karatsuba multiplication of two integers - [Link](https://github.com/aayushmnit/algorithms/blob/master/karatsuba_multiplication.py)
+2. Merge Sort for sorting a list of numbers - [Link](https://github.com/aayushmnit/algorithms/blob/master/merge_sort.py)
---
+++ merge_sort.py
@@ -0,0 +1,53 @@
+def MergeSort(x):
+    '''
+    Implementation of merge sort algorithm nlog(n) complexity
+    x : list
+    '''
+    # List of size 1 or empty list
+    if len(x) <= 1:
+        return x
+        # Divide
+
+    ## Find the middle point
+    n = int(len(x)/2)
+
+    y = x[:n]
+    z = x[n:]
+
+    # Recursion you beauty
+    y = MergeSort(y)
+    z = MergeSort(z)
+    
+    # Merge logic
+    finalOutput = []
+    
+    ## Caculating length of arrays to track upper bound
+    yLen = len(y)
+    zLen = len(z)
+
+    ## Initiate pointers
+    i, j = 0, 0
+
+    for k in range(len(x)):
+        # Check if first list is completely in final output
+        if (i+1) > yLen:
+            return finalOutput + z[j:]
+        # Check if second list is completely in final output
+        if (j+1) > zLen:
+            return finalOutput + y[i:]
+
+        ## Sort two numbers, if tie take from first array
+        if y[i] <= z[j]:
+            finalOutput.append(y[i])
+            i += 1
+        else:
+            finalOutput.append(z[j])   
+            j += 1          
+
+    return finalOutput
+
+
+
+    
+
+
